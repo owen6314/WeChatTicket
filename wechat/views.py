b371ecae4fd1a388737c483408b91a692d906fa0
owen <thss15_zhangqr@163.com>
2017-09-25 23:06:51 +0800

    math expression

--- wechat/views.py
+++ wechat/views.py
@@ -11,7 +11,7 @@ class CustomWeChatView(WeChatView):
     lib = WeChatLib(WECHAT_TOKEN, WECHAT_APPID, WECHAT_SECRET)
 
     handlers = [
-        HelpOrSubscribeHandler, UnbindOrUnsubscribeHandler, BindAccountHandler, BookEmptyHandler,
+        HelpOrSubscribeHandler, UnbindOrUnsubscribeHandler, BindAccountHandler, BookEmptyHandler,MathExpressionHandler
     ]
     error_message_handler = ErrorHandler
     default_handler = DefaultHandler
