051c474bc6eac8bff93c7c4e8fa417224596eb0a
owen <thss15_zhangqr@163.com>
2017-10-14 22:28:57 +0800

    image debug

--- adminpage/models.py
+++ adminpage/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 
 class Image(models.Model):
-    image = models.ImageField(upload_to='upload_img/', default="upload_img/nothing.jpg")
+    image = models.ImageField()
